379fb44094c829772587a1805967dcc345aa217c
kels0la <samelsola@gmail.com>
2018-07-30 20:02:52 -0500

    front end commit

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Data
 // ===========================================================
 
-
+let reservations = [];
+let waitlist = [];
 
 
 // Routes
@@ -25,7 +31,22 @@ app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "public/tables.html"));
 });
 
+// API Routes
+app.post('/api/reserve', function(req, res) {
+    // send back conrimation?
+    console.log('reservation received');
+    // if (reservations.length < 5) { add to reservations}
+    // else add to waitlist
+})
 
+app.get('/api/table', function(req, res) {
+    // return an array of all reservations
+    // send back json of reservations
+})
+
+app.get('/api/waitlist', function(req, res) {
+    // send back json of waitlist
+})
 
 
 // Listener
